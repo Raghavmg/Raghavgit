5d356ffdfec5be6ac4aad2b223a20032d7f784ad
venka <venka@ghorpade>
2023-06-18 16:40:57 +0530

    First commit...

---
+++ src/githubpro111/gitproo.java
@@ -0,0 +1,21 @@
+package githubpro111;
+
+public class gitproo {
+	public static void main(String[] args) {
+		String email="raghavmg123@gmail.com";
+		String password="ghorpade";
+		
+		if ((email=="raghavmg123@gmail.com")&&(password=="horpade")) {
+		System.out.println("login successfully");
+	
+			
+		}
+		else{
+			System.out.println("invalid user or password");
+		}
+	
+		}
+		
+	
+
+}
